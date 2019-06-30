5d141af76e12023215ec893b7fcfc7f67489ec6b
Sophie Gill <sophie-gill@hotmail.com>
2019-06-30 12:09:38 +0100

    handles single point strings

--- 20190630_scrabble_solver/__tests__/scrabbleSolver.test.js
+++ 20190630_scrabble_solver/__tests__/scrabbleSolver.test.js
@@ -3,14 +3,54 @@ const ScrabbleSolver = require('../src/scrabbleSolver');
 describe('ScrabbleSolver', () => {
   let scrabbleSolver;
   describe('#score', () => {
-    it('returns 0 when passed an empty string', () => {
-      scrabbleSolver = new ScrabbleSolver('');
-      expect(scrabbleSolver.score()).toEqual(0);
+    describe('zero score strings', () => {
+      test('empty string', () => {
+        scrabbleSolver = new ScrabbleSolver('');
+        expect(scrabbleSolver.score()).toEqual(0);
+      });
     });
 
-    it('returns 1 when passed a', () => {
-      scrabbleSolver = new ScrabbleSolver('a');
-      expect(scrabbleSolver.score()).toEqual(1)
+    describe('1 point letters', () => {
+      test('a', () => {
+        scrabbleSolver = new ScrabbleSolver('a');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('e', () => {
+        scrabbleSolver = new ScrabbleSolver('e');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('i', () => {
+        scrabbleSolver = new ScrabbleSolver('i');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('o', () => {
+        scrabbleSolver = new ScrabbleSolver('o');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('u', () => {
+        scrabbleSolver = new ScrabbleSolver('u');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('l', () => {
+        scrabbleSolver = new ScrabbleSolver('l');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('n', () => {
+        scrabbleSolver = new ScrabbleSolver('n');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('r', () => {
+        scrabbleSolver = new ScrabbleSolver('r');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('s', () => {
+        scrabbleSolver = new ScrabbleSolver('s');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
+      test('t', () => {
+        scrabbleSolver = new ScrabbleSolver('t');
+        expect(scrabbleSolver.score()).toEqual(1)
+      });
     });
   });
 });
